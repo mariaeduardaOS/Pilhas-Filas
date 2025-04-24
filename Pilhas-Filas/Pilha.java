8c7d419214eeffe461eaea22b39a99fd763140ab
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2025-04-24 20:34:06 -0300

    Update Pilha.java

--- Pilhas-Filas/Pilha.java
+++ Pilhas-Filas/Pilha.java
@@ -1,37 +1,42 @@
-import java.util.EmptyStackException;
+
+import java.util.ArrayList;
 
 public class Pilha<T> {
-    private T[] elementos;
-    private int topo;
+   private ArrayList<T> elementos;
+   private int capacidade;
 
-    @SuppressWarnings("unchecked")
-    public Pilha(int capacidade) {
-        elementos = (T[]) new Object[capacidade];
-        topo = -1;
-    }
+   public Pilha(int var1) {
+      this.capacidade = var1;
+      this.elementos = new ArrayList(var1);
+   }
 
-    public void empilhar(T elemento) {
-        if (topo == elementos.length - 1) {
-            throw new StackOverflowError("Pilha cheia");
-        }
-        elementos[++topo] = elemento;
-    }
+   public void empilhar(T var1) {
+      if (this.elementos.size() < this.capacidade) {
+         this.elementos.add(var1);
+      } else {
+         System.out.println("Pilha cheia! Não é possível empilhar mais elementos.");
+      }
 
-    public T desempilhar() {
-        if (estaVazia()) {
-            throw new EmptyStackException();
-        }
-        return elementos[topo--];
-    }
+   }
 
-    public boolean estaVazia() {
-        return topo == -1;
-    }
+   public T desempilhar() {
+      if (!this.estaVazia()) {
+         return this.elementos.remove(this.elementos.size() - 1);
+      } else {
+         System.out.println("Pilha vazia! Não há elementos para desempilhar.");
+         return null;
+      }
+   }
 
-    public T topo() {
-        if (estaVazia()) {
-            throw new EmptyStackException();
-        }
-        return elementos[topo];
-    }
+   public boolean estaVazia() {
+      return this.elementos.isEmpty();
+   }
+
+   public int getCapacidade() {
+      return this.capacidade;
+   }
+
+   public void limpar() {
+      this.elementos.clear();
+   }
 }
