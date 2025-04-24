b82f905668d5018afb10eefb9033c60a6c6e6b58
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2025-04-24 20:34:28 -0300

    Update Pilha.java

--- Pilhas-Filas/Pilha.java
+++ Pilhas-Filas/Pilha.java
@@ -1,42 +1,40 @@
-
 import java.util.ArrayList;
 
 public class Pilha<T> {
-   private ArrayList<T> elementos;
-   private int capacidade;
+    private ArrayList<T> elementos;
+    private int capacidade;
 
-   public Pilha(int var1) {
-      this.capacidade = var1;
-      this.elementos = new ArrayList(var1);
-   }
+    public Pilha(int capacidade) {
+        this.capacidade = capacidade;
+        this.elementos = new ArrayList<>(capacidade);
+    }
 
-   public void empilhar(T var1) {
-      if (this.elementos.size() < this.capacidade) {
-         this.elementos.add(var1);
-      } else {
-         System.out.println("Pilha cheia! Não é possível empilhar mais elementos.");
-      }
+    public void empilhar(T elemento) {
+        if (elementos.size() < capacidade) {
+            elementos.add(elemento);
+        } else {
+            System.out.println("Pilha cheia! Não é possível empilhar mais elementos.");
+        }
+    }
 
-   }
+    public T desempilhar() {
+        if (!estaVazia()) {
+            return elementos.remove(elementos.size() - 1);
+        } else {
+            System.out.println("Pilha vazia! Não há elementos para desempilhar.");
+            return null;
+        }
+    }
 
-   public T desempilhar() {
-      if (!this.estaVazia()) {
-         return this.elementos.remove(this.elementos.size() - 1);
-      } else {
-         System.out.println("Pilha vazia! Não há elementos para desempilhar.");
-         return null;
-      }
-   }
+    public boolean estaVazia() {
+        return elementos.isEmpty();
+    }
 
-   public boolean estaVazia() {
-      return this.elementos.isEmpty();
-   }
+    public int getCapacidade() {
+        return capacidade;
+    }
 
-   public int getCapacidade() {
-      return this.capacidade;
-   }
-
-   public void limpar() {
-      this.elementos.clear();
-   }
+    public void limpar() {
+        elementos.clear();
+    }
 }
